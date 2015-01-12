d04f063135f66b49e4a9a42b7c330baf505c51af
judngu <jtnguyen.jn@gmail.com>
2015-01-12 13:52:15 -0500

    added all other elements to events show form form

--- spec/features/user_creates_an_event_spec.rb
+++ spec/features/user_creates_an_event_spec.rb
@@ -8,16 +8,23 @@ describe "user create an event" do
 
     visit new_event_path
     fill_in "Name", with: "My Awesome Event"
+    fill_in "Address", with: "123 Space St."
     fill_in "Location", with: "Space"
     fill_in "City", with: "Rocketville"
     fill_in "State", with: "MA"
     fill_in "Question", with: "Who do you think is best at this meeting?"
-    fill_in "Passphrase", with: "This is my secret"
-    fill_in "Description", with: "This event weeds out how awesome people are"
+    fill_in "Passphrase", with: "This is my secret password"
+    fill_in "Description", with: "This event weeds out people who are underpreforming"
     click_on "Create Event"
 
     expect(page).to have_content("Event Sucessfully Created")
-    expect(page).to have_content("My Awesome Event")
+    expect(page).to have_content("123 Space St.")
+    expect(page).to have_content("Space")
+    expect(page).to have_content("Rocketville")
+    expect(page).to have_content("MA")
+    expect(page).to have_content("Who do you think is best at this meeting?")
+    expect(page).to have_content("This event weeds out people who are underpreforming")
+
   end
 
 end
