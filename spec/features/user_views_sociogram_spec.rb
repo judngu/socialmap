d57c58f526e8419a1fc52ca500c5d5492ca3496e
judngu <jtnguyen.jn@gmail.com>
2015-01-15 14:31:10 -0500

    modification to test

--- spec/features/user_views_sociogram_spec.rb
+++ spec/features/user_views_sociogram_spec.rb
@@ -26,12 +26,14 @@ describe "views a sociogram" do
     user2 = FactoryGirl.create(:user)
     user3 = FactoryGirl.create(:user)
     user4 = FactoryGirl.create(:user)
+    user5 = FactoryGirl.create(:user)
 
     attendee1 = FactoryGirl.create(:attendee, user_id: user1.id, event_id: event.id)
     attendee2 = FactoryGirl.create(:attendee, user_id: user2.id, event_id: event.id)
     attendee3 = FactoryGirl.create(:attendee, user_id: user3.id, event_id: event.id)
     attendee4 = FactoryGirl.create(:attendee, user_id: user4.id, event_id: event.id)
-
+    attendee5 = FactoryGirl.create(:attendee, user_id: user5.id, event_id: event.id)
+    
     sign_in(user1)
     visit event_path(event)
     within(:css, "#connection.value2") do
@@ -64,7 +66,15 @@ describe "views a sociogram" do
     end
     sign_out(user4)
 
-    expect(Pick.count).to eq 4
+    sign_in(user5)
+    visit event_path(event)
+    within(:css, "#connection.value1") do
+      select attendee3.user.name, from: "pick[picked_user_id]"
+      click_on "Create Pick"
+    end
+    sign_out(user5)
+
+    expect(Pick.count).to eq 5
 
     sign_in(user)
 
