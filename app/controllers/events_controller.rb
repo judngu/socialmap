0c6edfa69fd4f619f6124d2f41e2c21273f213de
judngu <jtnguyen.jn@gmail.com>
2015-01-19 16:39:58 -0500

    Clean up events controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,17 +4,16 @@ class EventsController < ApplicationController
   def show
     @event = Event.find_by(id: params[:id])
     @pick = Pick.new
-    @event_attendees = @event.users.all_except(current_user)
+    @event_attendees = @event.other_attendees(current_user)
   end 
 
   def new
     @event = Event.new
-    @rand_string = ('a'..'z').to_a.shuffle[0,5].join.upcase
   end
 
   def create
-    @event = Event.new(event_params)
-    @event.user = current_user
+    @event = current_user.events.build(event_params)
+
     if @event.save
       flash[:notice] = "Event sucessfully created."
       redirect_to event_path(@event)
@@ -29,7 +28,7 @@ class EventsController < ApplicationController
 
   def update
     @event = current_user.events.find(params[:id])
-    @user = @event.user
+
     if @event.update(event_params)
       redirect_to event_path(@event), notice: "Event sucessfully updated."
     else
@@ -39,14 +38,12 @@ class EventsController < ApplicationController
 
   def destroy
     @event = current_user.events.find(params[:id])
-      if @event.destroy
-        redirect_to root_path, notice: "Event sucessfully deleted."
-      else
-        redirect_to event_path, notice: "Event not deleted"
-      end
+    @event.destroy
+    redirect_to root_path, notice: "Event sucessfully deleted."
   end
 
   private
+  
   def event_params
     params.require(:event).permit(:name, :location, :address,:city, :state, :zipcode, :question, :passphrase, :description)
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,15 @@ class Event < ActiveRecord::Base
   has_many :users, through: :attendees
 
   validates :name, presence: true
-  validates :user_id, presence:true
+  validates :user, presence:true
   validates :question, presence: true
   validates :passphrase, presence: true, uniqueness: true
+
+  def other_attendees(user)
+  	users.all_except(user)
+  end
+
+  def self.random_passphrase
+    ('a'..'z').to_a.shuffle[0, 10].join.upcase
+  end
 end
