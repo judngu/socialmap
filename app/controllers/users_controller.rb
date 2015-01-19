a2b1ca9560cc23ead2bd55c1b3516b856530e3e3
judngu <jtnguyen.jn@gmail.com>
2015-01-19 14:31:12 -0500

    user profile only shows for current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user)
     @events = Event.where(user: @user)
     @attendee = Attendee.where(user: @user)
   end
--- spec/features/user_views_user_profile_spec.rb
+++ spec/features/user_views_user_profile_spec.rb
@@ -7,11 +7,21 @@ describe "views a user profile" do
     event = FactoryGirl.create(:event, user: user)
 
     sign_in(user)
-
     visit user_path(user)
 
     expect(page).to have_content(event.name)
     expect(page).to have_content(event.passphrase)
+  end
+
+  it "should only show a user profile for the current_user" do
+    user = FactoryGirl.create(:user)
+    user1 = FactoryGirl.create(:user)
+
+    sign_in(user1)
+    visit user_path(user)
+
+    expect(page).to have_content(user1.name)
+
   end
 
 end
