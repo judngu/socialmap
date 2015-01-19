75bc6dd3fd1ec8415217862202ac17157dcf12d7
judngu <jtnguyen.jn@gmail.com>
2015-01-19 17:24:04 -0500

    Move sociograph methods into Event model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,15 +14,57 @@ class Event < ActiveRecord::Base
   end
 
   def sociograph
-    nodes = Pick.nodes(self)
-
-    node_index = Pick.node_index(nodes)
-    links = Pick.links(self, node_index)
-
-    { nodes: nodes, links: links }
+    { nodes: grouped_users, links: user_links }
   end
 
   def self.random_passphrase
     ('a'..'z').to_a.shuffle[0, 10].join.upcase
+  end
+
+  private
+
+  def user_weights
+    weights = Hash.new(0)
+
+    picks.each do |pick|
+      weights[pick.picked_user.name] += pick.value
+    end
+
+    weights
+  end
+
+  def grouped_users
+    users.map do |user|
+      { "name" => user.name, "group" => random_color_group }
+    end
+  end
+
+  def users_index
+    @users_index ||= calculate_users_index
+  end
+
+  def calculate_users_index
+    user_index = Hash.new
+
+    grouped_users.each_with_index do |user, i|
+      name = user["name"]
+      user_index["#{name}"] = i
+    end
+
+    user_index
+  end
+
+  def user_links
+    picks.map do |pick|
+      {
+        "source" => users_index[pick.user.name],
+        "target" => users_index[pick.picked_user.name],
+        "value" => 1
+      }
+    end
+  end
+
+  def random_color_group
+    rand(50)
   end
 end
--- app/models/pick.rb
+++ app/models/pick.rb
@@ -3,46 +3,5 @@ class Pick < ActiveRecord::Base
   belongs_to :event
   belongs_to :picked_user, class_name: "User"
 
-  def self.node_weights(event)
-    user_scores = Hash.new(0)
 
-    event.picks.each do |pick|
-      user_scores[pick.picked_user.name] += pick.value
-    end
-
-    user_scores
-  end
-
-  def self.nodes(event)
-    event.users.map do |user|
-      { "name" => user.name, "group" => random_color_group }
-    end
-  end
-
-  def self.node_index(nodes)
-    node_index = Hash.new
-
-    nodes.each_with_index do |user, n|
-      name = user["name"]
-      node_index["#{name}"] = n
-    end
-
-    node_index
-  end
-
-  def self.links(event, node_index)
-    event.picks.map do |pick|
-      {
-        "source" => node_index[pick.user.name],
-        "target" => node_index[pick.picked_user.name],
-        "value" => 1
-      }
-    end
-  end
-
-  private
-
-  def self.random_color_group
-    rand(50)
-  end
 end
