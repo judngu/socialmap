6dd5712fcd1d9a29ea559e6a1ef8bd2a6e0684aa
judngu <jtnguyen.jn@gmail.com>
2015-01-18 23:42:54 -0500

    js cleanup

--- app/assets/javascripts/sociogram.js
+++ app/assets/javascripts/sociogram.js
@@ -59,35 +59,6 @@ node.append("text")
   .attr("dy", ".35em")
   .text(function(d) { return d.name });
 
-var padding = 10,
-  radius=8;
-
-function collide(alpha) {
-  var quadtree = d3.geom.quadtree(graph.nodes);
-  return function(d) {
-    var rb = 2*radius + padding,
-        nx1 = d.x - rb,
-        nx2 = d.x + rb,
-        ny1 = d.y - rb,
-        ny2 = d.y + rb;
-    quadtree.visit(function(quad, x1, y1, x2, y2) {
-      if (quad.point && (quad.point !== d)) {
-        var x = d.x - quad.point.x,
-            y = d.y - quad.point.y,
-            l = Math.sqrt(x * x + y * y);
-          if (l < rb) {
-          l = (l - rb) / l * alpha;
-          d.x -= x *= l;
-          d.y -= y *= l;
-          quad.point.x += x;
-          quad.point.y += y;
-        }
-      }
-      return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-    });
-  };
-}
-
 force.on("tick", function () {
   link.attr("x1", function (d) {
       return d.source.x;
@@ -115,6 +86,5 @@ force.on("tick", function () {
       .attr("y", function (d) {
       return d.y;
   });
-  node.each(collide(0.5));
 
 });
