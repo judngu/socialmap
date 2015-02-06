393a99fdb3ce186c2e74be78da0a0c48c3b20a89
judngu <jtnguyen.jn@gmail.com>
2015-02-05 19:09:17 -0500

    added table with vote count

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -4,4 +4,8 @@ class Attendee < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :event_id, presence: true
+
+  def pick_count
+    event.picks.where(picked_user: self).count
+  end
 end
